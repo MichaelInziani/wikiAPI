d3629730444db387dfa52b60a1e7cebef4b1ba3c
Michael Inziani <122669038+MichaelInziani@users.noreply.github.com>
2023-09-30 13:18:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const _ = require('lodash');
+const _ = require('lodash'); 
 
 const app = express();
 
