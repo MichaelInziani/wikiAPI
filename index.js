b7fd91e2313d217b9a31993750ad4b3525bd6585
Michael Inziani <mikeinziani004@gmail.com>
2023-09-30 13:17:00 +0300

    Made a few changes

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require('mongoose');
-const _ = require('lodash');
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import _ from "lodash";
+import "dotenv/config";
 
 const app = express();
-
-app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
 
-//TODO
 //CONNECT MONGOOSE
-main().catch(err => console.log(err));
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+
+    }
 }
+
+//main().catch(error => console.log(error));
+//async function main() {
+  //  await mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
+//}
 //mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
 
 //CREATE A SCHEMA
@@ -40,12 +50,12 @@ app.route("/articles")
     .get(async (req, res) => {
         try {
             const foundArticles = await Article.find();
-            // console.log(foundArticles);
+            console.log(foundArticles);
             res.send(foundArticles);
         }
-        catch (err) {
-            // console.log(err);
-            res.send(err);
+        catch (error) {
+            console.log(error.message);
+            res.send(error.message);
         }
     })
     .post(async (req, res) => {
@@ -56,23 +66,24 @@ app.route("/articles")
             })
             await newArticle.save();
             res.send("Successfuly added a new article.");
-            // console.log("Successfuly added a new article.");
+            console.log(newArticle);
+            console.log("Successfuly added a new article.");
         }
-        catch (err) {
-            res.send(err);
-            // console.log(err);
+        catch (error) {
+            res.send(error.message);
+            console.log(error.message);
         }
     })
     .delete(async (req, res) => {
         try {
             await Article.deleteMany({});
             res.send("Successfuly deleted all the articles.");
-            //console.log("Successfuly deleted all the articles.");
+            console.log("Successfuly deleted all the articles.");
 
         }
-        catch (err) {
-            res.send(err);
-            // console.log(err);
+        catch (error) {
+            res.send(error.message);
+            console.log(error.message);
         }
     });
 
@@ -91,9 +102,9 @@ app.route("/articles/:articleTitle")
                 res.send("No articles matching that title was found.");
             }
         }
-        catch (err) {
-            res.send(err);
-            console.log(err);
+        catch (error) {
+            res.send(error.message);
+            console.log(error.message);
 
             res.status(400).json({
                 message: "Something went wrong",
@@ -117,8 +128,8 @@ app.route("/articles/:articleTitle")
             } else {
                 res.send(`Article '${articleTitle}' not found`);
             }
-        } catch (err) {
-            res.send(err);
+        } catch (error) {
+            res.send(error.message);
         }
     })
 
@@ -133,8 +144,8 @@ app.route("/articles/:articleTitle")
                 message: "Article updated successfuly for:", title: req.body.title, content: req.body.content
             });
         }
-        catch (err) {
-            console.log(err);
+        catch (error) {
+            console.log(error.message);
             res.send("Update failed");
             res.status(400).json({
                 message: "Can't update the article",
@@ -149,11 +160,16 @@ app.route("/articles/:articleTitle")
             );
             res.send("Article deleted successfuly.");
         }
-        catch (err) {
-            res.send(err);
+        catch (error) {
+            res.send(error.message);
         }
     });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening for requests on port ${PORT}`);
+    });
 });
+//app.listen(3000, function () {
+   // console.log(`Listening for requests on port ${3000}`);
+//});
